def105e3ecada4e4eb326f826a64f6bfc071d9e7
Simon Wu (Infra SH) <simon.wu@smartnews.com>
2020-06-04 11:40:30 +0800

    add beforeAll afterAll test

--- src/test/kotlin/com/smartnews/springkotlinblogdemo/IntegrationTests.kt
+++ src/test/kotlin/com/smartnews/springkotlinblogdemo/IntegrationTests.kt
@@ -1,6 +1,8 @@
 package com.smartnews.springkotlinblogdemo
 
 import org.assertj.core.api.AssertionsForClassTypes.assertThat
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -11,6 +13,11 @@ import org.springframework.http.HttpStatus
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
 
+    @BeforeAll
+    fun setup() {
+        println(">> Setup")
+    }
+
     @Test
     fun `Assert blog page title, content and status code`() {
         val entity = restTemplate.getForEntity<String>("/")
@@ -18,4 +25,13 @@ class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
         assertThat(entity.body).contains("<h1>Blog</h1>")
     }
 
+    @Test
+    fun `Assert article page title, content and status code`() {
+        println(">> TODO")
+    }
+
+    @AfterAll
+    fun teardown() {
+        println(">> Tear down")
+    }
 }
