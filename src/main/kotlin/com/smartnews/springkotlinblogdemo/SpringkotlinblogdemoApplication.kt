f7600e54d89008670214302a428882e5f7bc3ae0
Simon Wu (Infra SH) <simon.wu@smartnews.com>
2020-06-04 13:47:51 +0800

    add application properties

--- src/main/kotlin/com/smartnews/springkotlinblogdemo/SpringkotlinblogdemoApplication.kt
+++ src/main/kotlin/com/smartnews/springkotlinblogdemo/SpringkotlinblogdemoApplication.kt
@@ -1,9 +1,11 @@
 package com.smartnews.springkotlinblogdemo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(BlogProperties::class)
 class SpringkotlinblogdemoApplication
 
 fun main(args: Array<String>) {
