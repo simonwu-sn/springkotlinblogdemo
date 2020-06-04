6d3cae9009773d81efe1b87f114fc1ab7b8340ff
Simon Wu (Infra SH) <simon.wu@smartnews.com>
2020-06-04 11:34:14 +0800

    Initial commit

---
+++ src/main/kotlin/com/smartnews/springkotlinblogdemo/SpringkotlinblogdemoApplication.kt
@@ -0,0 +1,11 @@
+package com.smartnews.springkotlinblogdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringkotlinblogdemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringkotlinblogdemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/smartnews/springkotlinblogdemo/SpringkotlinblogdemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.smartnews.springkotlinblogdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringkotlinblogdemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
